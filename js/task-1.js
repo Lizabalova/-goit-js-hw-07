93cb80a0fc1fd761d30a1af8061d47706566b2ed
Lizabalova <yelyzavetabalova@gmail.com>
2024-02-25 11:25:36 +0100

    update

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const list = document.querySelectorAll(".item");
+console.log(list);
+console.log(`Number of categories: ${list.length}`);
+const titles = document.querySelectorAll("h2");
+console.log(titles);
+titles.forEach (function (title) {
+    const  titleName = title.textContent;
+      console.log(`Category: ${titleName}`);
+    const categoriesNames = title.parentElement.querySelectorAll("ul > li");
+      console.log(`Elements: ${categoriesNames.length}`);
+});
